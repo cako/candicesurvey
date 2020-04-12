3fbf711f5a3c4bb8644564c5eab92f689f3f74b2
Carlos <c.dacostaf+github@gmail.com>
2020-04-11 21:10:47 -0300

    add react

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 5000;
 let usersRouter = require("./users");
 
 express()
-  .use("/users", usersRouter)
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
+  .use("/users", usersRouter)
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
---
+++ public/react/precache-manifest.3aa0edda72abf369ece17c2f72a3b419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27cf4649c64d5305a947ecd62ee03be2",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "99a5de44124ad044bab4",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "370c9fd060cfe3a4ac85",
+    "url": "/react/static/js/2.6d20342a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.6d20342a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a5de44124ad044bab4",
+    "url": "/react/static/js/main.bef7f611.chunk.js"
+  },
+  {
+    "revision": "7fb7b1380263496a5517",
+    "url": "/react/static/js/runtime-main.6de282b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
