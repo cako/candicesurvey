9f40c4b32767da27f631b4f0d59645967bf6e434
Carlos <c.dacostaf+github@gmail.com>
2020-04-11 23:02:25 -0300

    add react

---
+++ public/react/precache-manifest.4abc6a45635d71f454385cf61466ad25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d51b7d93dfa1d8add1d0622d0cab32",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "23e6128a7cacf6b96806",
+    "url": "/react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b933d2fb180b7170e28e",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23e6128a7cacf6b96806",
+    "url": "/react/static/js/2.d1ee9c5a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react/static/js/2.d1ee9c5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b933d2fb180b7170e28e",
+    "url": "/react/static/js/main.fbb450dd.chunk.js"
+  },
+  {
+    "revision": "d3bbd78a8d54fc698f69",
+    "url": "/react/static/js/runtime-main.03560b3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- public/react/service-worker.js
+++ public/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.3cecd36f8ced98af508b60e46b10e716.js"
+  "/react/precache-manifest.4abc6a45635d71f454385cf61466ad25.js"
 );
 
 self.addEventListener('message', (event) => {
